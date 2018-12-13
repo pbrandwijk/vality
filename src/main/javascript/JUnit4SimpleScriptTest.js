45404563238e146f06b0694d0164d00f6f1ec82c
Pieter Brandwijk <pieter@pbrandwijk.com>
2018-12-13 22:20:37 +0100

    #4 Add a simple script test

---
+++ src/main/javascript/JUnit4SimpleScriptTest.js
@@ -0,0 +1,10 @@
+
+// Create a Person object
+let person = new com.pbrandwijk.vality.model.Person("John", 25);
+
+// Change the name of the Person object
+person.setName("Tom");
+
+// Print out the name of the Person object
+print(person.getName());
+
---
+++ src/test/java/com/pbrandwijk/vality/JUnit4SimpleScriptTest.java
@@ -0,0 +1,36 @@
+package com.pbrandwijk.vality;
+
+import org.junit.Test;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class JUnit4SimpleScriptTest {
+
+    private static final String NASHORN_PROPERTY_NAME = "nashorn.args";
+    private static final String NASHORN_PROPERTY_VALUE = "--language=es6";
+    private static final String SCRIPT = "src/main/javascript/JUnit4SimpleScriptTest.js";
+    private static final String ENGINE_SHORT_NAME = "JavaScript";
+
+    private ScriptEngine engine;
+
+    @Test
+    public void runSimpleScript() throws FileNotFoundException, ScriptException {
+
+        // Set a system property to make sure Nashorn uses ECMAScript 6
+        System.setProperty(NASHORN_PROPERTY_NAME, NASHORN_PROPERTY_VALUE);
+        // Initialize a script engine manager
+        ScriptEngineManager factory = new ScriptEngineManager();
+        // Create JavaScript engine for the test
+        engine = factory.getEngineByName(ENGINE_SHORT_NAME);
+
+        // Evaluate the script
+        engine.eval(new FileReader(SCRIPT));
+
+    }
+
+}
