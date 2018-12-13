5600e12ff2d1076a82bbd233701d9a09887c0ea7
Pieter Brandwijk <pieter@pbrandwijk.com>
2018-12-13 22:24:01 +0100

    #4 Update simple script

--- src/main/javascript/JUnit4SimpleScriptTest.js
+++ src/main/javascript/JUnit4SimpleScriptTest.js
@@ -2,9 +2,12 @@
 // Create a Person object
 let person = new com.pbrandwijk.vality.model.Person("John", 25);
 
+// Print out the name of the Person object
+print("The name of the person is " + person.getName());
+
 // Change the name of the Person object
 person.setName("Tom");
 
-// Print out the name of the Person object
-print(person.getName());
+// Print out the new name
+print("The new name of the person is " + person.getName());
 
