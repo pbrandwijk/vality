437d6824dbf1432ef79e39e73ed6a3792512dafb
Pieter Brandwijk <pieter.brandwijk@capgemini.com>
2019-02-18 10:59:39 +0100

    #4 Move all examples to example package

--- src/main/javascript/JUnit4WithBindingsTest.js
+++
@@ -1,4 +0,0 @@
-logger.info("Starting script with person named " + person.getName());
-
-// Run the checkAge function from the library on the Person object
-checkAge(person);
--- src/main/javascript/library.js
+++
@@ -1,8 +0,0 @@
-
-function checkAge(person) {
-    if (person.getAge() != 33) {
-        logger.warn("I'm sorry, but your age is not " + person.getAge().toString());
-        logger.info("Setting age to 33");
-        person.setAge(33);
-    }
-}
